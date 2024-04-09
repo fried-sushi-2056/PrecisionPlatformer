5eaa29d0df3019cb618253a56040d244c44cd3aa
Tommy Turner <tommy_turner@asl.org>
2024-04-09 18:06:10 +0100

    Added block to change the scene

---
+++ PresPlatformer/Assets/NextLevelScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelScript : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    void OnTriggerEnter2D()
+    {
+        gameManager.LoadManScene();
+    }
+}
