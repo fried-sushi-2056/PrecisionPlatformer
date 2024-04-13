32e43dd49d516528b45218aa79eaf3f99bacbf08
Tommy Turner <tommy_turner@asl.org>
2024-04-13 22:46:54 +0100

    Small progress

--- PresPlatformer/Assets/TurretFire.cs
+++ PresPlatformer/Assets/TurretFire.cs
@@ -7,11 +7,13 @@ public class TurretFire : MonoBehaviour
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public Transform player;
+
     public float bulletSpeed = 10;
+    public bool canShoot = true;
 
     void Update(){
-        if(Mathf.Abs(bulletSpawnPoint.position.x-player.position.x) < 10 && Mathf.Abs(bulletSpawnPoint.position.y-player.position.y) < 10){
-            print("bullet primed");
+        if(Mathf.Abs(bulletSpawnPoint.position.x-player.position.x) < 10 && Mathf.Abs(bulletSpawnPoint.position.y-player.position.y) < 10 && canShoot){
+            canShoot = false;
             Fire();
         }
     }
@@ -19,5 +21,12 @@ public class TurretFire : MonoBehaviour
     void Fire(){
         var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
         bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.up * bulletSpeed;
+        StartCoroutine(Reload());
+        print(bulletSpawnPoint.up);
+    }
+
+    IEnumerator Reload(){
+        yield return new WaitForSeconds(2f);
+        canShoot = true;
     }
 }
