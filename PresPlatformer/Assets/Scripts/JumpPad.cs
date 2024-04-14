edccb11e3c401b92b5294ceed81bec4736604f00
Tommy Turner <tommy_turner@asl.org>
2024-04-14 21:14:21 +0100

    Made a door that needs player to push a button and some edits to level 1

--- PresPlatformer/Assets/Scripts/JumpPad.cs
+++ PresPlatformer/Assets/Scripts/JumpPad.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    private float bounce = 20f;
+    public float bounce = 20f;
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
